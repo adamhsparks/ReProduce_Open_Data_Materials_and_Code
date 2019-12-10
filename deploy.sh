1f6026e44e7152b090541eee28d09ca261c87985
Adam Sparks <adamhsparks@gmail.com>
2019-12-10 16:16:10 +1000

    Re-add deploy.sh that's gone missing!

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Your PATH is $PATH"
+echo "You are on branch $TRAVIS_BRANCH"
+echo "The working directory is $(pwd)"
+echo "The contents of contents/post are"
+ls content/post
+echo "The contents of contents/notes are"
+ls content/notes
+
+if [ "$TRAVIS_BRANCH" = "master" ]
+then
+	  echo "you are on master, deploying production."
+	  netlify deploy -a $NETLIFY_API --prod --dir=public
+else 
+    echo "you are not on master, deploying preview."
+    netlify deploy -a $NETLIFY_API --dir=public
+fi
