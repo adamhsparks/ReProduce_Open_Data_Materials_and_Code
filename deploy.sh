a1806c38336dfb29a7a6d7f969c2a0af66c664c1
Adam Sparks <adamhsparks@gmail.com>
2019-12-10 16:20:10 +1000

    Update deploy script to remove OPP-specific stuff

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,7 @@
 echo "Your PATH is $PATH"
 echo "You are on branch $TRAVIS_BRANCH"
 echo "The working directory is $(pwd)"
-echo "The contents of contents/post are"
-ls content/post
-echo "The contents of contents/notes are"
-ls content/notes
 
-if [ "$TRAVIS_BRANCH" = "master" ]
 then
 	  echo "you are on master, deploying production."
 	  netlify deploy -a $NETLIFY_API --prod --dir=public
